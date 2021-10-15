72c26fe6c4d9cff0c0ad9f72341c3b8cf04b631a
Tom Wiesing <tkw01536@gmail.com>
2021-10-15 17:26:23 +0200

    Add more testcases

--- reader_test.go
+++ reader_test.go
@@ -1,11 +1,88 @@
 package stringreader_test
 
 import (
+	"errors"
 	"fmt"
+	"reflect"
 	"strconv"
+	"testing"
 
 	"github.com/tkw1536/stringreader"
 )
+
+func TestMarshal_UnmarshalSingle(t *testing.T) {
+	m := &stringreader.Marshal{
+		NameTag:       "name",
+		ParserTag:     "parser",
+		DefaultParser: "always",
+		StrictNameTag: false,
+		StrictTyping:  false,
+	}
+
+	m.RegisterSingleParser("always", func(value string, ok bool) (interface{}, error) {
+		return value, nil
+	})
+	m.RegisterSingleParser("default", func(value string, ok bool) (interface{}, error) {
+		return "default", nil
+	})
+	m.RegisterSingleParser("special", func(value string, ok bool) (interface{}, error) {
+		return fmt.Sprintf("special:%q", value), nil
+	})
+	m.RegisterSingleParser("never", func(value string, ok bool) (interface{}, error) {
+		return nil, errors.New("never parser")
+	})
+
+	// Type is a type used for tests
+	type Type struct {
+		NoTag            string
+		ParserTag        string `parser:"always"`
+		NameTag          string `name:"name"`
+		ParserAndNameTag string `parser:"special" name:"special"`
+	}
+
+	tests := []struct {
+		name     string
+		m        map[string]string
+		wantDest Type
+		wantErr  bool
+	}{
+		{
+			m: map[string]string{
+				"NoTag":     "no-tag",
+				"ParserTag": "parser-tag",
+				"name":      "name-tag",
+				"special":   "special value",
+			},
+			wantDest: Type{
+				NoTag:            "no-tag",
+				ParserTag:        "parser-tag",
+				NameTag:          "name-tag",
+				ParserAndNameTag: "special:\"special value\"",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var dest Type
+			err := m.UnmarshalSingle(&dest, stringreader.SourceMap(tt.m))
+			if tt.wantErr {
+				if err == nil {
+					t.Error("wantErr = true, err = nil")
+				}
+				return
+			}
+
+			if err != nil {
+				t.Errorf("Marshal.UnmarshalSingle() err = %s, want = nil", err.Error())
+			}
+
+			if !reflect.DeepEqual(dest, tt.wantDest) {
+				t.Errorf("Marshal.UnmarshalSingle() dest = %v, want = %v", dest, tt.wantDest)
+			}
+		})
+	}
+}
 
 func ExampleMarshal_UnmarshalSingle() {
 
@@ -15,8 +92,6 @@ func ExampleMarshal_UnmarshalSingle() {
 
 		ParserTag:     "type",
 		DefaultParser: "string",
-
-		StrictTyping: false,
 	}
 
 	// UserProfile is an example struct to be unmarshaled below.
