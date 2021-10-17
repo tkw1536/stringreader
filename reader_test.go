1666ffce15e3a784ff6941f1ed8867d6198d1744
Tom Wiesing <tkw01536@gmail.com>
2021-10-17 09:41:08 +0200

    More tests

--- reader_test.go
+++ reader_test.go
@@ -11,61 +11,95 @@ import (
 )
 
 func TestMarshal_UnmarshalSingle(t *testing.T) {
-	m := &stringreader.Marshal{
-		NameTag:       "name",
-		ParserTag:     "parser",
-		DefaultParser: "always",
-		StrictNameTag: false,
-		StrictTyping:  false,
-	}
 
-	m.RegisterSingleParser("always", func(value string, ok bool) (interface{}, error) {
-		return value, nil
-	})
-	m.RegisterSingleParser("default", func(value string, ok bool) (interface{}, error) {
-		return "default", nil
-	})
-	m.RegisterSingleParser("special", func(value string, ok bool) (interface{}, error) {
-		return fmt.Sprintf("special:%q", value), nil
-	})
-	m.RegisterSingleParser("never", func(value string, ok bool) (interface{}, error) {
-		return nil, errors.New("never parser")
-	})
+	// setupparser resets the parsers for m
+	reset_parsers := func(m *stringreader.Marshal) {
+		m.SingleParsers = nil
+		m.MultiParsers = nil
 
-	// Type is a type used for tests
-	type Type struct {
-		NoTag            string
-		ParserTag        string `parser:"always"`
-		NameTag          string `name:"name"`
-		ParserAndNameTag string `parser:"special" name:"special"`
+		m.RegisterSingleParser("always", func(value string, ok bool) (interface{}, error) {
+			return value, nil
+		})
+		m.RegisterSingleParser("default", func(value string, ok bool) (interface{}, error) {
+			return "default", nil
+		})
+		m.RegisterSingleParser("special", func(value string, ok bool) (interface{}, error) {
+			return fmt.Sprintf("special:%q", value), nil
+		})
+		m.RegisterSingleParser("never", func(value string, ok bool) (interface{}, error) {
+			return nil, errors.New("never parser")
+		})
 	}
 
 	tests := []struct {
 		name     string
-		m        map[string]string
-		wantDest Type
+		marshal  stringreader.Marshal // ignores the existing fields
+		src      map[string]string
+		wantDest interface{}
 		wantErr  bool
 	}{
 		{
-			m: map[string]string{
+			name: "complete test",
+			marshal: stringreader.Marshal{
+				NameTag:       "name",
+				ParserTag:     "parser",
+				DefaultParser: "always",
+				StrictNameTag: false,
+				StrictTyping:  false,
+			},
+			src: map[string]string{
 				"NoTag":     "no-tag",
 				"ParserTag": "parser-tag",
 				"name":      "name-tag",
 				"special":   "special value",
 			},
-			wantDest: Type{
+			wantDest: struct {
+				NoTag            string
+				ParserTag        string `parser:"always"`
+				NameTag          string `name:"name"`
+				ParserAndNameTag string `parser:"special" name:"special"`
+			}{
 				NoTag:            "no-tag",
 				ParserTag:        "parser-tag",
 				NameTag:          "name-tag",
 				ParserAndNameTag: "special:\"special value\"",
 			},
 		},
+		{
+			name: "no default parser skips untagged fields",
+			marshal: stringreader.Marshal{
+				NameTag:       "name",
+				ParserTag:     "parser",
+				StrictNameTag: false,
+				StrictTyping:  false,
+			},
+			src: map[string]string{
+				"ParserTag": "parser-tag-value",
+				"name":      "name-tag-value",
+				"special":   "special-value",
+			},
+			wantDest: struct {
+				NoTag            string
+				NameTag          string `name:"name"`
+				ParserTag        string `parser:"always"`
+				ParserAndNameTag string `parser:"special" name:"special"`
+			}{
+				ParserTag:        "parser-tag-value",
+				ParserAndNameTag: "special:\"special-value\"",
+			},
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			var dest Type
-			err := m.UnmarshalSingle(&dest, stringreader.SourceMap(tt.m))
+			m := tt.marshal
+			reset_parsers(&m)
+
+			// create a new element of the dest type!
+			target := reflect.TypeOf(tt.wantDest)
+			dest := reflect.New(target)
+
+			err := m.UnmarshalSingle(dest.Interface(), stringreader.SourceMap(tt.src))
 			if tt.wantErr {
 				if err == nil {
 					t.Error("wantErr = true, err = nil")
@@ -77,8 +111,9 @@ func TestMarshal_UnmarshalSingle(t *testing.T) {
 				t.Errorf("Marshal.UnmarshalSingle() err = %s, want = nil", err.Error())
 			}
 
-			if !reflect.DeepEqual(dest, tt.wantDest) {
-				t.Errorf("Marshal.UnmarshalSingle() dest = %v, want = %v", dest, tt.wantDest)
+			gotDest := dest.Elem().Interface()
+			if !reflect.DeepEqual(gotDest, tt.wantDest) {
+				t.Errorf("Marshal.UnmarshalSingle() dest = %v, want = %v", gotDest, tt.wantDest)
 			}
 		})
 	}
